ed9178a24dbc64436479c8c748bf9e4bdf12b330
Ravipatel02 <87352218+Ravipatel02@users.noreply.github.com>
2022-04-01 00:28:29 +0530

    otp
    otp css and otp js

---
+++ otp.js
@@ -0,0 +1,33 @@
+let h1 = document.getElementById('count');
+
+    function counter(){
+
+        let i = 300;
+
+       let id = setInterval(function(){
+            i--
+            h1.innerText = i;
+            if(i<0){
+                clearInterval(id);
+                return i
+            }
+        } , 1000)
+    }
+
+    counter()
+
+    var otp = document.querySelector("#hi12").addEventListener('submit',show);
+    function show(e){
+        event.preventDefault()
+        let otp = document.querySelector("#otp").value ;
+        //console.log(otp)
+        if(otp=="123456"){
+            alert("Order Successfully Place")
+            window.location.href="payment.html"
+        }
+        else{
+            alert("Plese Enter correct OTP.")
+        }
+    }
+    
+  
